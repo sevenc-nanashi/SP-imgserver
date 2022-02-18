67b3e6a1e37a71c9333f3a61fe8c28fb02b1a768
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2022-02-18 22:35:40 +0900

    Add: Extraの生成を追加

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import os
 from PIL import Image
 from io import BytesIO
 import aiohttp
-from quart import Quart, send_file
+from quart import Quart, request, send_file
 import pjsekai_background_gen_pillow as pjbg
 
 app = Quart(__name__)
 SUPPORTED_FORMATS = ["png", "jpg", "jpeg", "webp"]
 generator: pjbg.Generator = pjbg.Generator()
+extra_generator: pjbg.Generator = pjbg.Generator(Image.open("./background-base-extra.png"))
 
 
 @app.get("/")
@@ -37,17 +38,21 @@ async def generate(target: str):
         target += ".png"
     name = target.split(".")[0]
     ext = target.split(".")[1]
+    uniq = name
+    extra = request.args.get("extra") == "true"
+    if extra:
+        uniq += "-extra"
     if ext not in SUPPORTED_FORMATS:
         return {
             "message": "Unsupported format",
             "supported": SUPPORTED_FORMATS,
         }, 400
-    if os.path.exists(f"dist/{name}.{ext}"):
+    if os.path.exists(f"dist/{uniq}.{ext}"):
         print("Already exists, using cached version")
-    elif os.path.exists(f"dist/{name}.png"):
+    elif os.path.exists(f"dist/{uniq}.png"):
         print("Already exists with png, converting from png")
-        im = Image.open(f"dist/{name}.png").convert("RGB")
-        im.save(f"dist/{name}.{ext}")
+        im = Image.open(f"dist/{uniq}.png").convert("RGB")
+        im.save(f"dist/{uniq}.{ext}")
     else:
         print("Generating...")
         base_name = name.removesuffix(".extra")
@@ -61,13 +66,17 @@ async def generate(target: str):
                 data = await resp.read()
                 jacket = Image.open(BytesIO(data))
         loop = asyncio.get_event_loop()
-        res = (await loop.run_in_executor(None, generator.generate, jacket))
-        res.save(f"dist/{name}.png")
+        if extra:
+            gen = extra_generator
+        else:
+            gen = generator
+        res = (await loop.run_in_executor(None, gen.generate, jacket))
+        res.save(f"dist/{uniq}.png")
         if ext != "png":
-            res.save(f"dist/{name}.{ext}")
+            res.save(f"dist/{uniq}.{ext}")
 
     return await send_file(
-        f"dist/{name}.{ext}",
+        f"dist/{uniq}.{ext}",
     )
 
 
