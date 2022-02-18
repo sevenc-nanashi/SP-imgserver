4ec1abe58fa0e63c4aa010fddea4f3539a7229bd
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2022-02-19 08:19:44 +0900

    Add: Add generating with file upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+from hashlib import sha256
 import os
 from PIL import Image
 from io import BytesIO
@@ -33,7 +34,7 @@ async def index():
 
 
 @app.route("/generate/<string:target>", methods=["GET", "POST"])
-async def generate(target: str):
+async def generate_swpt(target: str):
     if "." not in target:
         target += ".png"
     name = target.split(".")[0]
@@ -70,7 +71,49 @@ async def generate(target: str):
             gen = extra_generator
         else:
             gen = generator
-        res = (await loop.run_in_executor(None, gen.generate, jacket))
+        res = await loop.run_in_executor(None, gen.generate, jacket)
+        res.save(f"dist/{uniq}.png")
+        if ext != "png":
+            res.save(f"dist/{uniq}.{ext}")
+
+    return await send_file(
+        f"dist/{uniq}.{ext}",
+    )
+
+
+@app.route("/generate", methods=["POST"])
+async def generate():
+    if 'file' not in await request.files:
+        return {
+            "message": "No file were uploaded"
+        }, 400
+    ext = request.args.get("ext", "png")
+    if ext not in SUPPORTED_FORMATS:
+        return {
+            "message": "Unsupported format",
+            "supported": SUPPORTED_FORMATS,
+        }, 400
+    file = (await request.files)['file']
+    extra = request.args.get("extra") == "true"
+    uniq = sha256(file.read()).hexdigest()
+    if extra:
+        uniq += "-extra"
+
+    if os.path.exists(f"dist/{uniq}.{ext}"):
+        print("Already exists, using cached version")
+    elif os.path.exists(f"dist/{uniq}.png"):
+        print("Already exists with png, converting from png")
+        im = Image.open(f"dist/{uniq}.png").convert("RGB")
+        im.save(f"dist/{uniq}.{ext}")
+    else:
+        print("Generating...")
+        jacket = Image.open(file.stream)
+        loop = asyncio.get_event_loop()
+        if extra:
+            gen = extra_generator
+        else:
+            gen = generator
+        res = await loop.run_in_executor(None, gen.generate, jacket)
         res.save(f"dist/{uniq}.png")
         if ext != "png":
             res.save(f"dist/{uniq}.{ext}")
