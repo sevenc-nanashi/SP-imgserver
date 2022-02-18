6aea665c8f332c77f67dd4d16e5ae44e394a31c0
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2022-02-18 22:23:23 +0900

    Change: Move core to pjsekai_background_gen_pillow

--- main.py
+++ main.py
@@ -1,46 +1,14 @@
+import asyncio
 import os
-from PIL import Image, ImageOps
+from PIL import Image
 from io import BytesIO
 import aiohttp
 from quart import Quart, send_file
-from hashlib import sha256
-
-assets_dir = os.path.dirname(__file__) + "/assets"
-base_normal = Image.open(assets_dir + "/background-base.png")
-mask_img = Image.open(assets_dir + "/mask.png").convert("L")
-side_mask = Image.open(assets_dir + "/side-mask.png")
-dot_tile = Image.open(assets_dir + "/dot-tilea.png")
-
-
-def get_asset_hash():
-    sha = sha256()
-    with open(assets_dir + "/background-base.png", "rb") as f:
-        sha.update(f.read())
-    with open(assets_dir + "/mask.png", "rb") as f:
-        sha.update(f.read())
-    with open(assets_dir + "/side-mask.png", "rb") as f:
-        sha.update(f.read())
-    with open(assets_dir + "/dot-tilea.png", "rb") as f:
-        sha.update(f.read())
-    return sha.hexdigest()
-
-
-def alpha(img, al):
-    imga = img.copy()
-    imga.putalpha(round(255 * al))
-    return imga
-
-
-class Deformer:
-    def __init__(self, dist):
-        self.dist = dist
-
-    def getmesh(self, im):
-        return [((0, 0, *im.size), self.dist)]
-
+import pjsekai_background_gen_pillow as pjbg
 
 app = Quart(__name__)
 SUPPORTED_FORMATS = ["png", "jpg", "jpeg", "webp"]
+generator: pjbg.Generator = pjbg.Generator()
 
 
 @app.get("/")
@@ -63,7 +31,7 @@ async def index():
     }
 
 
-@app.post("/generate/<string:target>")
+@app.route("/generate/<string:target>", methods=["GET", "POST"])
 async def generate(target: str):
     if "." not in target:
         target += ".png"
@@ -74,12 +42,12 @@ async def generate(target: str):
             "message": "Unsupported format",
             "supported": SUPPORTED_FORMATS,
         }, 400
-    if os.path.exists(f"dist/{name}-{get_asset_hash()}.{ext}"):
+    if os.path.exists(f"dist/{name}.{ext}"):
         print("Already exists, using cached version")
-    elif os.path.exists(f"dist/{name}-{get_asset_hash()}.png"):
-        print("Already exists, converting from png")
-        im = Image.open(f"dist/{name}-{get_asset_hash()}.png").convert("RGB")
-        im.save(f"dist/{name}-{get_asset_hash()}.{ext}")
+    elif os.path.exists(f"dist/{name}.png"):
+        print("Already exists with png, converting from png")
+        im = Image.open(f"dist/{name}.png").convert("RGB")
+        im.save(f"dist/{name}.{ext}")
     else:
         print("Generating...")
         base_name = name.removesuffix(".extra")
@@ -92,74 +60,14 @@ async def generate(target: str):
                     }, 404
                 data = await resp.read()
                 jacket = Image.open(BytesIO(data))
-        jacket.convert("RGBA")
-        base = base_normal.copy().convert("RGBA")
-        base2 = Image.new("RGBA", base.size, (0, 0, 0, 0))
-        base3 = Image.new("RGBA", base.size, (0, 0, 0, 0))
-        shift = -30
-        base3.paste(
-            alpha(
-                ImageOps.deform(
-                    jacket.resize((703, 708)),
-                    deformer=Deformer((0, -18, -6, 720, 704, 719, 704, 2)),
-                ),
-                0.8,
-            ),
-            (449, 98),
-        )
-        base3.paste(
-            alpha(
-                ImageOps.deform(
-                    jacket.resize((618, 706)),
-                    deformer=Deformer((1, -43, -5, 707, 621, 721, 616, 2)),
-                ),
-                0.8,
-            ),
-            (1019, 48),
-        )
-        shift = 10
-        base2.paste(
-            alpha(
-                ImageOps.deform(
-                    jacket,
-                    deformer=Deformer(
-                        (0, 0, -shift, jacket.height, jacket.width + shift, jacket.height, jacket.width, 0)
-                    ),
-                ).resize((470, 450)),
-                0.8,
-            ),
-            (787, 189),
-        )
-        base2.paste(
-            alpha(
-                ImageOps.deform(
-                    jacket,
-                    deformer=Deformer(
-                        (0, jacket.height, 0, 0, jacket.width, 0, jacket.width, jacket.height)
-                    ),
-                ).resize((450, 450)),
-                0.7,
-            ),
-            (797, 683),
-        )
-
-        # base.save(dir + f"/../dist/bg/{name}.png")
-        # base.paste(base2, (0, 0), mask=mask_img)
-        buffer = Image.alpha_composite(base, base2)
-        buffer.paste(base3, (0, 0), mask=side_mask)
-        buffer = Image.alpha_composite(buffer, dot_tile)
-        res = Image.new("RGBA", mask_img.size)
-        diff = (buffer.height - mask_img.height) // 2
-        # print(buffer.crop((0, diff, base.width, base.height - diff)).size, mask_img.size)
-        res.paste(base.crop((0, diff, base.width, base.height - diff - 1)), (0, 0))
-        res.paste(buffer.crop((0, diff, base.width, base.height - diff - 1)), (0, 0), mask=mask_img)
-        res = res.convert("RGB")
-        res.save(f"dist/{name}-{get_asset_hash()}.png")
+        loop = asyncio.get_event_loop()
+        res = (await loop.run_in_executor(None, generator.generate, jacket))
+        res.save(f"dist/{name}.png")
         if ext != "png":
-            res.save(f"dist/{name}-{get_asset_hash()}.{ext}")
+            res.save(f"dist/{name}.{ext}")
 
     return await send_file(
-        f"dist/{name}-{get_asset_hash()}.{ext}",
+        f"dist/{name}.{ext}",
     )
 
 
